0ace545f477c7d71c49efb5d01cd59f08ddb7966
Kelechi Ugwu <kcblack22@gmail.com>
2024-12-22 00:15:16 +0100

    Project setup

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import prisma from './utils/prismaInstance';
+import router from './routes';
+
+const app = express();
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8001;
+app.use(express.json());
+
+app.use(router);
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('SIGINT', async () => {
+    await prisma.$disconnect();
+    server.close(() => {
+      console.log('Server stopped');
+    });
+    process.exit(0);
+  });
